74ea9e775c2bb27ad0025e079492e41307cc8e5c
mawangdan <mawangdan@foxmail.com>
2024-06-05 23:32:29 +0800

    Initial commit

---
+++ AE/data_loader.py
@@ -0,0 +1,201 @@
+import h5py
+import torch
+import torch.utils.data as data
+import os
+import pickle
+import numpy as np
+import trimesh
+from PIL import Image
+import os.path
+import random
+import pybullet as p
+import pybullet_data
+import open3d as o3d
+
+p.connect(p.DIRECT)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+robotId = p.loadURDF("./franka_description/robots/panda_arm.urdf")  # KUKA_IIWA_URDF | DRAKE_IIWA_URDF
+def Point(x=0., y=0., z=0.):
+    return np.array([x, y, z])
+def quat_from_euler(euler):
+    return p.getQuaternionFromEuler(euler)
+def Euler(roll=0., pitch=0., yaw=0.):
+    return np.array([roll, pitch, yaw])
+def Pose(point=None, euler=None):
+    point = Point() if point is None else point
+    euler = Euler() if euler is None else euler
+    return (point, quat_from_euler(euler))
+
+CLIENT = 0
+def set_pose(body, pose):
+    (point, quat) = pose
+    p.resetBasePositionAndOrientation(body, point, quat, physicsClientId=CLIENT)
+
+
+def sample_points_from_sphere(center, radius, num_points):
+    u = np.random.rand(num_points)
+    v = np.random.rand(num_points)
+    theta = 2 * np.pi * u
+    phi = np.arccos(2 * v - 1)
+    r = radius * np.cbrt(np.random.rand(num_points))
+    x = r * np.sin(phi) * np.cos(theta) + center[0]
+    y = r * np.sin(phi) * np.sin(theta) + center[1]
+    z = r * np.cos(phi) + center[2]
+    return np.vstack((x, y, z)).T
+
+
+def sample_points_from_box(center, half_extents, num_points):
+    x = np.random.uniform(center[0] - half_extents[0], center[0] + half_extents[0], num_points)
+    y = np.random.uniform(center[1] - half_extents[1], center[1] + half_extents[1], num_points)
+    z = np.random.uniform(center[2] - half_extents[2], center[2] + half_extents[2], num_points)
+    return np.vstack((x, y, z)).T
+
+
+def sample_points_from_cylinder(center, radius, height, num_points):
+    theta = np.random.uniform(0, 2 * np.pi, num_points)
+    z = np.random.uniform(center[2] - height / 2, center[2] + height / 2, num_points)
+    r = radius * np.sqrt(np.random.rand(num_points))
+    x = r * np.cos(theta) + center[0]
+    y = r * np.sin(theta) + center[1]
+    return np.vstack((x, y, z)).T
+
+
+def get_point_cloud_from_urdf(urdf_path, num_points,center):
+    # Connect to PyBullet in DIRECT mode (no GUI)
+    p.connect(p.DIRECT)
+
+    # Load the URDF model
+    robot_id = p.loadURDF(urdf_path)
+    set_pose(robot_id,
+             Pose(Point(x=center[0], y=center[1], z=center[2]), Euler()))
+    # Get collision shape data
+    collision_shape_data = p.getVisualShapeData(robot_id)
+
+    # Extract points from the geometry
+    all_points = []
+    for shape in collision_shape_data:
+        geom_type = shape[2]
+        origin = shape[5]
+        if geom_type == p.GEOM_SPHERE:
+            radius = shape[3][0]
+            points = sample_points_from_sphere(origin, radius, num_points // len(collision_shape_data))
+        elif geom_type == p.GEOM_BOX:
+            half_extents = shape[3]
+            points = sample_points_from_box(origin, half_extents, num_points // len(collision_shape_data))
+        elif geom_type == p.GEOM_CYLINDER:
+            radius = shape[3][0]
+            height = shape[3][1]
+            points = sample_points_from_cylinder(origin, radius, height, num_points // len(collision_shape_data))
+        elif geom_type == p.GEOM_MESH:
+            vertices = p.getMeshData(robot_id, shape[1])[1]
+            triangles = p.getMeshData(robot_id, shape[1])[3]
+            mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(vertices),
+                                             triangles=o3d.utility.Vector3iVector(triangles))
+            sampled_points = mesh.sample_points_uniformly(number_of_points=num_points // len(collision_shape_data))
+            points = np.asarray(sampled_points.points)
+        else:
+            continue
+
+        all_points.append(points)
+
+    # Combine all points
+    combined_points = np.vstack(all_points)
+
+    # Randomly sample required number of points
+    if len(combined_points) > num_points:
+        indices = np.random.choice(len(combined_points), num_points, replace=False)
+        sampled_points = combined_points[indices]
+    else:
+        sampled_points = combined_points
+
+    p.disconnect()
+    return sampled_points
+
+def save_point_cloud(NP=1800):
+
+
+    with h5py.File(f"./train_data-63.hdf5", "r") as f:
+        m_length = f["m_length"][:]
+        N = int(m_length.__len__())
+        obstacles_point_list = np.zeros((N, 1400 * 3), dtype=np.float32)
+        solutions = f["solutions"][:]
+        sphere_centers = f["sphere_centers"][:]
+        sphere_radiis = f["sphere_radiis"][:]
+        cube_centers = f["cube_centers"][:]
+        cube_sizes = f["cube_sizes"][:]
+        # 获取机械臂的关节数
+        num_joints = 7
+        end_effector_positions=np.zeros((N,300, 3), dtype=np.float32)
+        # 打印所有关节信息
+        for i in range(num_joints):
+            print(p.getJointInfo(robotId, i))
+        for i in range(0,m_length.__len__()):
+            for k in range(0,min(300,int(m_length[i][0]))):
+                for j in range(num_joints):
+                    p.resetJointState(robotId, j, solutions[i][k][j])
+                # 获取末端执行器的位置和方向
+                link_state = p.getLinkState(robotId, num_joints - 1)
+                end_effector_positions[i][k][0]= link_state[4][0] # 末端执行器的位置
+                end_effector_positions[i][k][1]= link_state[4][1] # 末端执行器的位置
+                end_effector_positions[i][k][2]= link_state[4][2] # 末端执行器的位置
+
+        for i in range(0,m_length.__len__()):
+            print(i)
+            obstacles_point=[]
+
+
+            for j in range(0,2):
+                if abs(sphere_radiis[i][j] - 0.2) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../medium_sphere.urdf", 700, sphere_centers[i][j]))
+                if abs(sphere_radiis[i][j] - 0.1) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../small_sphere.urdf", 700, sphere_centers[i][j]))
+                if abs(cube_sizes[i][j] - 0.2) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../medium_cube.urdf", 700, cube_centers[i][j]))
+
+            obstacles_point_list[i]=np.concatenate((obstacles_point[0], obstacles_point[1]), axis=1).flatten()
+        with h5py.File(f"point_cloud.hdf5", "w-") as f:
+            tmp = f.create_dataset("obstacles_point_list", (N, 1400*3))
+            tmp[...]=obstacles_point_list[...]
+            tmp = f.create_dataset("solutions", (N, 300,7))
+            tmp[...] = solutions[...]
+            tmp = f.create_dataset("m_length", (N, 1))
+            tmp[...] = m_length[...]
+            tmp = f.create_dataset("end_effector_positions", (N,300, 3))
+            tmp[...] = end_effector_positions[...]
+
+def load_dataset(N=6075,NP=1800):
+    obstacles_point_list = np.zeros((N, 1400*3), dtype=np.float32)
+    with h5py.File(f"train_data.hdf5", "r") as f:
+        m_length = f["m_length"][:]
+        solutions = f["solutions"][:]
+        sphere_centers = f["sphere_centers"][:]
+        sphere_radiis = f["sphere_radiis"][:]
+        cube_centers = f["cube_centers"][:]
+        cube_sizes = f["cube_sizes"][:]
+        for i in range(0,m_length.__len__()):
+            print(i)
+            obstacles_point=[]
+
+
+            for j in range(0,2):
+                if abs(sphere_radiis[i][j] - 0.2) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../medium_sphere.urdf", 700, sphere_centers[i][j]))
+                if abs(sphere_radiis[i][j] - 0.1) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../small_sphere.urdf", 700, sphere_centers[i][j]))
+                if abs(cube_sizes[i][j] - 0.2) <0.01:
+                    obstacles_point.append(
+                        get_point_cloud_from_urdf("../medium_cube.urdf", 700, cube_centers[i][j]))
+
+            obstacles_point_list[i]=np.concatenate((obstacles_point[0], obstacles_point[1]), axis=1).flatten()
+    return 	obstacles_point_list
+def visualize_point_cloud(points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    o3d.visualization.draw_geometries([pcd])
+if __name__ == '__main__':
+    save_point_cloud()
---
+++ data_loader.py
@@ -0,0 +1,152 @@
+import h5py
+import torch
+import os
+import numpy as np
+import os.path
+import random
+from torch.autograd import Variable
+import torch.nn as nn
+
+# Environment Encoder
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.demention = 3
+        self.encoder = nn.Sequential(nn.Linear(1400 * self.demention, 786), nn.PReLU(), nn.Linear(786, 512), nn.PReLU(),
+                                     nn.Linear(512, 256), nn.PReLU(), nn.Linear(256, 60))
+
+    def forward(self, x):
+        x = self.encoder(x)
+        return x
+
+
+
+
+# N=number of environments; NP=Number of Paths
+def load_dataset(N=135, NP=45):
+    Q = Encoder()
+    Q.load_state_dict(torch.load('./models/cae_encoder.pkl'))
+    if torch.cuda.is_available():
+        Q.cuda()
+
+    with h5py.File(f"./AE/point_cloud.hdf5", "r") as f:
+        m_length = f["m_length"][:]
+        N = int(m_length.__len__()/NP)
+        obstacles_point_list = f["obstacles_point_list"][:]
+        solutions = f["solutions"][:]
+        end_effector_positions=f["end_effector_positions"][:]
+        obs_rep = np.zeros((N, 60), dtype=np.float32)
+        for i in range(0, N,45):
+            # load obstacle point cloud
+            # temp = np.fromfile('../../dataset/obs_cloud/obc' + str(i) + '.dat')
+            # temp = temp.reshape(len(temp) / 2, 2)
+            # obstacles = np.zeros((1, 2800), dtype=np.float32)
+            # obstacles[0] = temp.flatten()
+            inp = torch.from_numpy(obstacles_point_list[i])
+            #inp = Variable(inp).cuda()
+            output = Q(inp)
+            output = output.data.cpu()
+            obs_rep[i] = output.numpy()
+
+    ## calculating length of the longest trajectory
+    max_length = 300
+    path_lengths = np.zeros((N, NP), dtype=np.int64)
+    for i in range(0, N):
+        for j in range(0, NP):
+            # fname = '../../dataset/e' + str(i) + '/path' + str(j) + '.dat'
+            # if os.path.isfile(fname):
+            #     path = np.fromfile(fname)
+            #     path = path.reshape(len(path) / 2, 2)
+            #     path_lengths[i][j] = len(path)
+            path_lengths[i][j]=min(300,int(m_length[i*45+j][0]))
+
+    paths = np.zeros((N, NP, max_length, 7), dtype=np.float32)  ## padded paths
+
+    for i in range(0, N):
+        for j in range(0, NP):
+            # fname = '../../dataset/e' + str(i) + '/path' + str(j) + '.dat'
+            # if os.path.isfile(fname):
+            #     path = np.fromfile(fname)
+            #     path = path.reshape(len(path) / 2, 2)
+            paths[i,j,:max_length,:]=solutions[i*45+j,:max_length,:]
+
+    dataset = []
+    targets = []
+    for i in range(0, N):
+        for j in range(0, NP):
+            if path_lengths[i][j] > 0:
+                for m in range(0, path_lengths[i][j] - 1):
+                    data = np.zeros(60+7+7, dtype=np.float32)
+                    data[0:60] = obs_rep[i, :]
+                    data[60:60+7]=paths[i,j,m,:]
+                    data[60+7:60+7+7] = paths[i,j,path_lengths[i][j] - 1,:]
+
+                    targets.append(paths[i][j][m + 1])
+                    dataset.append(data)
+
+    data = list(zip(dataset, targets))
+    random.shuffle(data)
+    dataset, targets = zip(*data)
+    return np.asarray(dataset), np.asarray(targets)
+
+
+# N=number of environments; NP=Number of Paths; s=starting environment no.; sp=starting_path_no
+# Unseen_environments==> N=10, NP=2000,s=100, sp=0
+# seen_environments==> N=100, NP=200,s=0, sp=4000
+def load_test_dataset(N=100, NP=200, s=0, sp=4000):
+    obc = np.zeros((N, 7, 2), dtype=np.float32)
+    temp = np.fromfile('../../dataset/obs.dat')
+    obs = temp.reshape(len(temp) / 2, 2)
+
+    temp = np.fromfile('../../dataset/obs_perm2.dat', np.int32)
+    perm = temp.reshape(77520, 7)
+
+    ## loading obstacles
+    for i in range(0, N):
+        for j in range(0, 7):
+            for k in range(0, 2):
+                obc[i][j][k] = obs[perm[i + s][j]][k]
+
+    Q = Encoder()
+    Q.load_state_dict(torch.load('../models/cae_encoder.pkl'))
+    if torch.cuda.is_available():
+        Q.cuda()
+
+    obs_rep = np.zeros((N, 28), dtype=np.float32)
+    k = 0
+    for i in range(s, s + N):
+        temp = np.fromfile('../../dataset/obs_cloud/obc' + str(i) + '.dat')
+        temp = temp.reshape(len(temp) / 2, 2)
+        obstacles = np.zeros((1, 2800), dtype=np.float32)
+        obstacles[0] = temp.flatten()
+        inp = torch.from_numpy(obstacles)
+        inp = Variable(inp).cuda()
+        output = Q(inp)
+        output = output.data.cpu()
+        obs_rep[k] = output.numpy()
+        k = k + 1
+    ## calculating length of the longest trajectory
+    max_length = 0
+    path_lengths = np.zeros((N, NP), dtype=np.int8)
+    for i in range(0, N):
+        for j in range(0, NP):
+            fname = '../../dataset/e' + str(i + s) + '/path' + str(j + sp) + '.dat'
+            if os.path.isfile(fname):
+                path = np.fromfile(fname)
+                path = path.reshape(len(path) / 2, 2)
+                path_lengths[i][j] = len(path)
+                if len(path) > max_length:
+                    max_length = len(path)
+
+    paths = np.zeros((N, NP, max_length, 2), dtype=np.float32)  ## padded paths
+
+    for i in range(0, N):
+        for j in range(0, NP):
+            fname = '../../dataset/e' + str(i + s) + '/path' + str(j + sp) + '.dat'
+            if os.path.isfile(fname):
+                path = np.fromfile(fname)
+                path = path.reshape(len(path) / 2, 2)
+                for k in range(0, len(path)):
+                    paths[i][j][k] = path[k]
+
+    return obc, obs_rep, paths, path_lengths
